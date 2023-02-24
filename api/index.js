d5d66cb5f9975e8b77d9c6ab97ec5e74a3e182f9
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-02-24 06:46:02 +0530

    initial-commit

---
+++ api/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import helmet from "helmet";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+// import mentorRoute from "./routes/mentors.js";
+// import sessionRoute from "./routes/sessions.js";
+// import authRoute from "./routes/auth.js";
+
+const app = express();
+app.use(helmet());
+
+const port = process.env.PORT || 8080;
+mongoose.set("strictQuery", true);
+const connect = async () => {
+  try {
+    await mongoose
+      .connect(process.env.URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("connected"))
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// mongoose.connect(
+//   process.env.URI,
+//   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+//   () => console.log("connected to mongo")
+// );
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// app.use("/api/mentor", mentorRoute);
+// app.use("/api/session", sessionRoute);
+// app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.listen(port, () => {
+  connect();
+  console.log("Server listening on port http://localhost:" + port);
+});
